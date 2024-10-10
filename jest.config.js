23ac847ed00f7333c71081eb40f4c7e4e22fee40
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2024-10-10 15:51:41 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
-// jest.config.js
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom', // Simulate browser environment for DOM tests
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/app'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Setup file for jest-dom
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  collectCoverageFrom: ['<rootDir>/app/**/*.{ts,tsx}'],
-  coverageDirectory: '<rootDir>/coverage',
+import type {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
 };
+
+export default config;
